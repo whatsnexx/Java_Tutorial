bea87c09628912cadb7bcfa78afc8efa52851051
paulsmelser <paul.smelser@gmail.com>
2013-03-15 12:01:18 -0400

    romoving local host testing static block

--- src/script/Main.java
+++ src/script/Main.java
@@ -19,21 +19,6 @@ import org.apache.catalina.util.Base64;
 
 
 public class Main {
-
-	static {
-		// for localhost testing only	
-		javax.net.ssl.HttpsURLConnection
-				.setDefaultHostnameVerifier(new javax.net.ssl.HostnameVerifier() {
-
-					public boolean verify(String hostname,
-							javax.net.ssl.SSLSession sslSession) {
-						if (hostname.equals("localhost")) {
-							return true;
-						}
-						return false;
-					}
-				});
-	}
 	
 	public static void main(String[] args){
 
